d4d1a48e601f0cba4df867789631875b6a5dd68b
writer169 <148839750+writer169@users.noreply.github.com>
2025-02-28 11:51:31 +0600

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-// Временная отладка для проверки переменных окружения
-console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET);
-console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
-console.log("ALLOWED_EMAILS:", process.env.ALLOWED_EMAILS);
-
 export default NextAuth({
   providers: [
     GoogleProvider({
