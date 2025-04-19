86d9cafb58ccae64b794173b6bdc69c776f3e487
writer169 <148839750+writer169@users.noreply.github.com>
2025-04-19 14:22:33 +0600

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -2,12 +2,14 @@
 export const DEVICE_TYPES = {
   SOCKET: 'socket',
   AIR_SENSOR: 'airSensor',
+  THERMOMETER: 'thermometer',
   UNKNOWN: 'unknown'
 };
 
 export const DEVICE_IDS = {
   SOCKETS: ["bf86db6ccc98ec3f63ac0d", "bfcb457736e5303fe8ao1u", "bf014aa7c8cd67fc7fq7ck"],
-  AIR_SENSOR: "bf496ddae64215bd93p0qr"
+  AIR_SENSOR: "bf496ddae64215bd93p0qr",
+  THERMOMETERS: ["bff8525efa6adbe724woag", "bf4496436cae831338sptn"]
 };
 
 export const REFRESH_INTERVAL = 60000; // 1 minute in milliseconds
@@ -24,6 +26,7 @@ export const PARAM_LABELS = {
   timer: "Таймер",
   temperature: "Температура",
   humidity: "Влажность",
+  battery: "Батарея",
   co2: "CO₂",
   voc: "ЛОС",
   ch2o: "Формальдегид"
